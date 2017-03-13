ae2898afdb6de57c51853753b932af84eddda06a
PTC\odsptc10 <serkoodessa@gmail.com>
2017-03-13 16:27:50 +0200

    v0.1

--- lesson1/lesson1/lesson1.cpp
+++ lesson1/lesson1/lesson1.cpp
@@ -7,11 +7,26 @@
 using namespace std;
 
 
+int naiveSearch(int arr[], int size, int key)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] == key)
+			return i;
+	}
 
+	return -1;
+}
 
 int main()
 {
-	cout << "hello" << endl;
+	int arr1[] = { 0, 8, 11, 60, 4, 1, 5, 9 };
+
+
+	cout << naiveSearch(arr1, 8, 8) << endl;
+	cout << naiveSearch(arr1, 8, 5) << endl;
+
+	system("pause");
     return 0;
 }
 
