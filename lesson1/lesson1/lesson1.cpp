c4e2a6d15576595a2fd0100a10559a3be8b19af8
PTC\odsptc10 <serkoodessa@gmail.com>
2017-03-16 16:52:33 +0200

    next

--- lesson1/lesson1/lesson1.cpp
+++ lesson1/lesson1/lesson1.cpp
@@ -1,32 +1,115 @@
 // lesson1.cpp : Defines the entry point for the console application.
 //
 
-
 #include "stdafx.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
+template <class T>
+ostream& operator<<(ostream& o, const vector<T>& v) {
+	o << "[";
+	for (auto& x : v) {
+		o << x << ", ";
+	}
+	return o << "]";
+}
 
-int naiveSearch(int arr[], int size, int key)
-{
-	for (int i = 0; i < size; i++)
-	{
-		if (arr[i] == key)
+template <class TFunc, class TResult, class TParam1>
+void test(TResult expect, TFunc f, TParam1 p1) {
+	auto got = f(p1);
+	if (got != expect) {
+		cerr << "failed: " << expect << " != " << got << endl;
+	}
+}
+
+template <class TFunc, class TResult, class TParam1, class TParam2>
+void test(TResult expect, TFunc f, TParam1 p1, TParam2 p2) {
+	auto got = f(p1, p2);
+	if (got != expect) {
+		cerr << "failed: f(" << p1 << ", " << p2 << ") = "
+			<< got << " != " << expect << endl;
+	}
+}
+
+int search_0(int v[], size_t size, int key) {
+	for (int i = 0; i < size; ++i) {
+		if (v[i] == key) {
 			return i;
+		}
 	}
 
 	return -1;
 }
 
-int main()
-{
-	int arr1[] = { 0, 8, 11, 60, 4, 1, 5, 9 };
+int search_1(int v[], size_t size, int key) {
+	v[size] = key;
 
+	int i = 0;
+	while (v[i] != key) { ++i; }
 
-	cout << naiveSearch(arr1, 8, 8) << endl;
-	cout << naiveSearch(arr1, 8, 5) << endl;
+	if (i != size) {
+		return i;
+	}
 
-	system("pause");
-    return 0;
+	return -1;
 }
 
+int search_2(const std::vector<int>& v, int key) {
+	for (int i = 0; i < v.size(); ++i) {
+		if (v[i] == key) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+int search_3(std::vector<int>& v, int key) {
+	v.push_back(key);
+	int i = 0;
+	while (v[i] != key) {
+		++i;
+	}
+	v.pop_back();
+
+	if (i != v.size()) {
+		return i;
+	}
+
+	return -1;
+}
+
+
+void test_search() {
+
+	typedef vector<int> Array;
+
+	auto search = search_2;
+	auto key = 8;
+
+	// key not exists in array
+	test(-1, search, Array(), key); // degerate
+	test(-1, search, Array({ key - 1 }), key); // trivial
+	test(-1, search, Array({ key - 1, key + 1 }), key); // trivial
+	test(-1, search, Array({ 1, 2, 3, 4, 5, 7 }), key); // general
+	test(-1, search, Array({ 9, 10, 11, 12 }), key); // general
+	test(-1, search, Array({ 4, 1, 2, 7, 10 }), key); // general
+	// key exists in array
+	// non appliable // degerate
+	test(0, search, Array({ key - 1 }), key); // trivial
+	test(0, search, Array({ key - 1, key + 1 }), key); // trivial
+	test(-1, search, Array({ 1, 2, 3, 4, 5, 7 }), key); // general
+	test(-1, search, Array({ 9, 10, 11, 12 }), key); // general
+	test(-1, search, Array({ 4, 1, 2, 7, 10 }), key); // general
+
+	//test(6, search_2, Array({ 0,1,2,3,4,5,6,7,8 }), 8);
+}
+
+
+int main(int argc, char const *argv[])
+{
+	test_search();
+	system("pause");
+	return 0;
+}
